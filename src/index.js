3ad179d082d5ccfa2708d20237825d14cd286fbb
Zhigulskiy <evgeniy1st@users.noreply.github.com>
2020-06-20 14:16:52 +0300

    добавит prop-types и начальные моки

--- src/index.js
+++ src/index.js
@@ -2,6 +2,54 @@ import ReactDOM from "react-dom";
 import React from "react";
 import App from "./components/app.jsx";
 
+const Films = [
+  {
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+    image: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`
+  },
+
+  {
+    title: `Bohemian Rhapsody`,
+    image: `img/bohemian-rhapsody.jpg`
+  },
+
+  {
+    title: `Macbeth`,
+    image: `img/macbeth.jpg`
+  },
+
+  {
+    title: `Aviator`,
+    image: `img/aviator.jpg`
+  },
+
+  {
+    title: `We need to talk about Kevin`,
+    image: `img/we-need-to-talk-about-kevin.jpg`
+  },
+
+  {
+    title: `What We Do in the Shadows`,
+    image: `img/what-we-do-in-the-shadows.jpg`
+  },
+
+  {
+    title: `Revenant`,
+    image: `img/revenant.jpg`
+  },
+
+  {
+    title: `Johnny English`,
+    image: `img/johnny-english.jpg`
+  },
+
+  {
+    title: `Shutter Island`,
+    image: `img/shutter-island.jpg`
+  },
+
+];
+
 const init = () => {
   const PromoFilm = {
     TITLE: `The Grand Budapest Hotel`,
@@ -11,9 +59,10 @@ const init = () => {
 
   ReactDOM.render(
       <App
-        title={PromoFilm.TITLE}
-        genre={PromoFilm.GENRE}
-        year={PromoFilm.YEAR}
+        promoTitle={PromoFilm.TITLE}
+        promoGenre={PromoFilm.GENRE}
+        promoYear={PromoFilm.YEAR}
+        films={Films}
       />,
       document.querySelector(`#root`)
   );
